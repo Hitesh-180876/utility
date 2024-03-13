1572f8ba254e59b679e681e81e95caa3cec996c5
Hitesh-180876 <sharma.hiteshd@gmail.com>
2024-03-14 00:10:56 +0530

    simple test

--- src/test/java/com/example/utility/ControllerTest.java
+++ src/test/java/com/example/utility/ControllerTest.java
@@ -1,11 +1,20 @@
 package com.example.utility;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ControllerTest {
 
     @Test
+    @DisplayName("This is my test case name")
     public void testSayHi(){
+        String hit = "Hitesh";
+
+//        assertTrue(hit.equals("Jk"));
+        Assertions.assertThat(hit).isEqualTo("Hitesh");
 
     }
 }
