b6552be4d66ef2687d1e04b8b7421f66c1826ade
Hitesh-180876 <sharma.hiteshd@gmail.com>
2024-03-24 00:47:55 +0530

    zipkin distributed traces with logger

--- src/main/java/com/example/utility/controller/Controller.java
+++ src/main/java/com/example/utility/controller/Controller.java
@@ -1,5 +1,8 @@
 package com.example.utility.controller;
 
+import io.micrometer.observation.annotation.Observed;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,11 +15,19 @@ public class Controller {
     @Autowired
     private WebClient.Builder webClientBuilder;
 
-
+    Logger logger = LoggerFactory.getLogger(Controller.class);
     @GetMapping("/hi")
+    @Observed(
+            name = "user.name",
+            contextualName = "demo-service",
+            lowCardinalityKeyValues = {"userType", "userType2"}
+    )
     public Mono<String> sayHi(){
         String url = "http://localhost:8080/hi-demo";
+        logger.info(url);
         Mono<String> response =
+                webClientBuilder.build().get().uri(url).retrieve().bodyToMono(String.class);
+        Mono<String> response1 =
                 webClientBuilder.build().get().uri(url).retrieve().bodyToMono(String.class);
         return response;
     }
